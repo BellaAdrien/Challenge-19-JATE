528b3c1e22ed15cf710d26e9c98e7e3392a9836b
BellaAdrien <Iadrien2020@fau.edu>
2023-10-30 16:53:38 -0400

    save19

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,20 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const postDb = async (content) => {
+  console.log('Post to the ase');
+  const todosDb = await openDB('todos', 1);
+  const tx = todosDb.transaction('todos', 'readwrite');
+  const store = tx.objectStore('todos');
+  const request = store.add({ todo: content });
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+
+};
 
 initdb();
