cc7459f62bf7beca3c0564d92b2f62ebee22b86c
BellaAdrien <Iadrien2020@fau.edu>
2023-10-30 17:05:46 -0400

    save19

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,12 +13,12 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (id, content) => {
+export const putDb = async (content) => {
   console.log('PUT to the database');
-  const todosDb = await openDB('todos', 1);
-  const tx = todosDb.transaction('todos', 'readwrite');
-  const store = tx.objectStore('todos');
-  const request = store.put({ id: id, todo: content });
+  const jateDb = await openDB('JATE', 1);
+  const tx = jateDb.transaction('JATE', 'readwrite');
+  const store = tx.objectStore('JATE');
+  const request = store.put({ id:1, JATE: content });
   const result = await request;
   console.log('Data saved to the database', result);
 };
@@ -27,9 +27,9 @@ export const putDb = async (id, content) => {
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET all from the database');
-  const todosDb = await openDB('todos', 1);
-  const tx = todosDb.transaction('todos', 'readonly');
-  const store = tx.objectStore('todos');
+  const jateDb = await openDB('JATE', 1);
+  const tx = jateDb.transaction('JATE', 'readonly');
+  const store = tx.objectStore('JATE');
   const request = store.getAll();
   const result = await request;
   console.log('result.value', result);
