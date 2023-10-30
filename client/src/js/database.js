b2679ee927dd004672f67bc11b6570354ae6ef49
BellaAdrien <Iadrien2020@fau.edu>
2023-10-30 17:00:14 -0400

    save19

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,12 +13,12 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const postDb = async (content) => {
-  console.log('Post to the ase');
+export const putDb = async (id, content) => {
+  console.log('PUT to the database');
   const todosDb = await openDB('todos', 1);
   const tx = todosDb.transaction('todos', 'readwrite');
   const store = tx.objectStore('todos');
-  const request = store.add({ todo: content });
+  const request = store.put({ id: id, todo: content });
   const result = await request;
   console.log('Data saved to the database', result);
 };
@@ -26,7 +26,14 @@ export const postDb = async (content) => {
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-
+  console.log('GET all from the database');
+  const todosDb = await openDB('todos', 1);
+  const tx = todosDb.transaction('todos', 'readonly');
+  const store = tx.objectStore('todos');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
 };
 
 initdb();
